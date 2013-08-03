6e1a0dd44f0361b77d00473bfad13c640b8f2eb7
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2013-08-03 13:40:13 +0200

    Add missing c file

---
+++ mandelbrot/mandelbrot.c
@@ -0,0 +1,43 @@
+#include <Python.h>
+#include <stdio.h>
+
+#define BAILOUT 16
+#define MAX_ITERATIONS 1000
+
+int _mandelbrot(double x, double y)
+{
+	double cr = y - 0.5;
+	double ci = x;
+	double zi = 0.0;
+	double zr = 0.0;
+	int i = 0;
+
+	while(1) {
+		i ++;
+		double temp = zr * zi;
+		double zr2 = zr * zr;
+		double zi2 = zi * zi;
+		zr = zr2 - zi2 + cr;
+		zi = temp + temp + ci;
+		if (zi2 + zr2 > BAILOUT)
+			return i;
+		if (i > MAX_ITERATIONS)
+			return 0;
+	}
+
+}
+
+void draw() {
+	int x,y;
+	for (y = -39; y < 39; y++) {
+		printf("\n");
+		for (x = -39; x < 39; x++) {
+			int i = _mandelbrot(x/40.0, y/40.0);
+			if (i==0)
+				printf("*");
+			else
+				printf(" ");
+		}
+	}
+	printf ("\n");
+}
